abf819e39d15fb5c9b411c6aa1157455b1cda0d3
anasenadir <121679280+anasenadir@users.noreply.github.com>
2023-12-01 00:29:30 +0100

    fix 102-magic_calculation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,12 +2,12 @@
 
 def magic_calculation(a, b):
 	"""Match bytecode"""
-    from magic_calculation_102 import add, sub
+	from magic_calculation_102 import add, sub
 
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return(c)
+	if a < b:
+		c = add(a, b)
+		for i in range(4, 6):
+			c = add(c, i)
+		return(c)
 
-    return(sub(a, b))
+	return(sub(a, b))
