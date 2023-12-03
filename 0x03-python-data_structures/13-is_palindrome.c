7aec7aaa49bc5c4aef5ab4d5cdb4dd9c7136fb0f
root <root@DESKTOP-PDMJJRJ.localdomain>
2023-12-03 23:08:04 +0100

    fix some errors 2

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,62 +1,33 @@
 #include "lists.h"
 
 /**
- * get_len - get the length of a linked list
- * @head: the head pointer
- * Return: the length of the list
+ * is_palindrome - function that checks if a singly linked list is a,
+ * palindrome.
+ * @head: double pointer to head of the list.
+ *
+ * Return: 1 if given list is a palindrome. 0 otherwise.
  */
-int get_len(listint_t *head)
-{
-	listint_t *current = head;
-	int len = 0;
-
-	while (current)
-	{
-		len += 1;
-		current = current->next;
-	}
-	return (len);
-}
-
-
-/**
- * is_palindrome - function that checks if a
- * singly linked list is a palindrome
- * @head: the head pointer
- * Return: 0 if it is not a palindrome
- *          1 if it is a palindrome
-*/
 int is_palindrome(listint_t **head)
 {
-	int *arr_ptr, list_len, i, j;
-	listint_t *current = *head;
+	int arr[10000];
+	int i, n = 0;
+	listint_t *traverse;
 
-	list_len = get_len(*head);
-	if (list_len == 0)
-		return (1);
+	if (head == NULL)
+		return (0);
 
-	arr_ptr = malloc(list_len + 1);
-	
-	if (!arr_ptr)
-		return (1);
-	
-	for (i = 0; i < list_len; i++)
+	/* copy numbers from linked list to arr */
+	traverse = *head;
+	while (traverse)
 	{
-		arr_ptr[i] = current->n;
-		current = current->next;
+		arr[n++] = traverse->n;
+		traverse = traverse->next;
 	}
-
-	i = 0;
-	j = list_len - 1;
-
-	while (i < j)
+	/* check if arr is a palindrome */
+	for (i = 0; i < n / 2; i++)
 	{
-		if (arr_ptr[i] != arr_ptr[j])
+		if (arr[i] != arr[n - i - 1])
 			return (0);
-		i += 1;
-		j -= 1;
 	}
-
-	free(arr_ptr);
 	return (1);
 }
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,10 +7,9 @@ def print_matrix_integer(matrix=[[]]):
     You are not allowed to cast integers into strings
     You have to use str.format() to print integers
     """
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            s = " " if j != len(matrix) - 1 else ''
-            print("{:d}".format(matrix[i][j]), end=s)
-            # if j != len(matrix) - 1:
-                # print("", end=' ')
-        print("")
+    for row in range(len(matrix)):
+        for column in range(len(matrix[row])):
+            if column != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[row][column]), end='')
+        print()
