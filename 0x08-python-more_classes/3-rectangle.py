8427b55012d2fcf746fc499f912f416efb734134
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-05 00:56:35 +0100

    update the perimeter method

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -88,7 +88,7 @@ class Rectangle:
         Returns:
             int: the rectangle perimeter
         """
-        if (self.__width == 0 and self.__height == 0):
+        if (self.__width == 0 or self.__height == 0):
             return 0
         return 2 * (self.__width + self.__height)
 
