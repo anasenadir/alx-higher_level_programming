a8a2a71423828431e7f2bea684393ad1c141a6f6
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-05 00:52:57 +0100

    update the documentation of the methods

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -5,7 +5,8 @@ This module containe a class that define a rectangle.
 
 
 class Rectangle:
-    """class Rectangle that defines a rectangle"""
+    """class Rectangle that defines a rectangle
+    """
     def __init__(self, width=0, height=0) -> None:
         """the constractor method of the class
 
@@ -18,21 +19,33 @@ class Rectangle:
 
     @property
     def width(self):
-        """return the width value"""
+        """return the width value
+
+        Returns:
+            int: width value
+        """
         return self.__width
 
     @property
     def height(self):
-        """return the height value"""
+        """return the height value
+
+        Returns:
+            int: height value
+        """
         return self.__height
 
     @width.setter
     def width(self, value):
-        """set the width of the rectangle
-        (Args):
-            value (int): the new width value
-        """
+        """set the value of the width
 
+        Args:
+            value (int): the new width value
+
+        Raises:
+            TypeError: If `value` is not an int.
+            ValueError: If `value` is less than 0.
+        """
         if (not isinstance(value, int)):
             raise TypeError("width must be an integer")
 
@@ -45,8 +58,12 @@ class Rectangle:
     def height(self, value):
         """set the value of the height
 
-        (Args):
+        Args:
             value (int): the new height value
+
+        Raises:
+            TypeError: If `value` is not an int.
+            ValueError: If `value` is less than 0.
         """
         if (not isinstance(value, int)):
             raise TypeError("height must be an integer")
@@ -57,20 +74,30 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Public instance method that returns the rectangle area"""
+        """Public instance method that returns the rectangle area
+
+        Returns:
+            int: return the rect area
+        """
         return self.__width * self.height
 
     def perimeter(self):
         """method that returns the rectangle perimeter
         Perimeter of Rectangle is:
-            -> 2 (width + height)
+
+        Returns:
+            int: the rectangle perimeter
         """
         if (self.__width == 0 and self.__height == 0):
             return 0
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """print the rectangle with the character #"""
+        """print the rectangle with the character #
+
+        Returns:
+            str: rectangle with the character #
+        """
         rect = ""
         for _ in range(self.__height):
             rect += "#" * self.width + "\n"
