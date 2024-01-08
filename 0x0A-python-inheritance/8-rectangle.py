c4566752862132877000362a33f79512c789256c
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-09 00:32:24 +0100

    fix same mistaks

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,11 +10,10 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 class Rectangle(BaseGeometry):
     """BaseGeometry class"""
-    
+
     def __init__(self, width, height):
         """initialize new Geometry instance"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-        
