fda20245ddb06696e13f604bed8288a81eac5058
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-09 00:46:15 +0100

    fix some errors

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,5 +6,11 @@ Rectangle = __import__("9-rectangle").Rectangle
 class Square(Rectangle):
     """Square Class"""
     def __init__(self, size):
-        super().__init__(size, size)
+        """initialize the square class"""
+        self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Method that returns area of a square"""
+        return self.__size ** 2
