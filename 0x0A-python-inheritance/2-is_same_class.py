fa4c82322c0cc657f7ebb4794861515dbcd63e9f
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-08 22:22:51 +0100

    fix the he 'is_same_class' function

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,6 +4,7 @@ object is exactly an instance of the specified class ;
 otherwise False.
 """
 
+
 def is_same_class(obj, a_class):
     """function that returns True if the
     object is exactly an instance of the specified class ;
@@ -13,14 +14,6 @@ def is_same_class(obj, a_class):
         obj (obj): _description_
         a_class (class): _description_
     """
-    if (type(obj) ==  a_class):
+    if (type(obj) == a_class):
         return True
     return False
-
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))
