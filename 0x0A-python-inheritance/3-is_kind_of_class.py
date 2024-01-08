553901e3aac22372982821b8b95bbfa38ea904b6
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-08 22:28:28 +0100

    create the function 'is_kind_of_class'

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """function that returns True if the object is an instance
-of, or if the object is an instance of a 
+of, or if the object is an instance of a
 class that inherited from, the specified class ;
 otherwise False.
 """
@@ -8,7 +8,7 @@ otherwise False.
 
 def is_kind_of_class(obj, a_class):
     """function that returns True if the object is an instance
-    of, or if the object is an instance of a 
+    of, or if the object is an instance of a
     class that inherited from, the specified class ;
     otherwise False.
 
