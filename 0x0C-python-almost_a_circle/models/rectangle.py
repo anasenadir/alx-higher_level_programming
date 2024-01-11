3bca14bc4ecc5d6e9a6e7a54cb68510a4eb83d0b
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-11 18:37:42 +0100

    Update the class Rectangle by adding the public method def update(self, *args): that assigns an argument to each attribute:

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -125,3 +125,12 @@ class Rectangle(Base):
             self.id, self.__x,
             self.__y, self.__width, self.__height
         )
+
+    
+    def update(self, *args):
+        """assigns an argument to each attribute
+        """
+        params = ["id", "width", "height", "x", "y"]
+        for index, attr in enumerate(args):
+            if index != len(params):
+                setattr(self, params[index], attr)
