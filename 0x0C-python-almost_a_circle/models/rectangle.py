cd0579cd6ca14574d8d80f8b8ce210d49ca7e1a7
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-12 17:16:07 +0100

    fix the pycodestyle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -145,4 +145,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """return the dictionary represintation of the react object"""
-        return {"x": self.x , "y": self.y, "id": self.id, "height": self.height, "width": self.width}
+        return {"x": self.x, "y": self.y, "id": self.id,
+                "height": self.height, "width": self.width}
