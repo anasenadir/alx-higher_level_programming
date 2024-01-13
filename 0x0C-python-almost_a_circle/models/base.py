aca9afd92b2a8ecc14390fa4b0c16d3350eff23f
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-13 14:30:41 +0100

    fix the pycodestyle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,8 +26,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
