f9c221158415d6e62ee181bdf6251b2c8c2d09dd
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-13 15:27:23 +0100

    update the save_to_file classmethod

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,20 +31,20 @@ class Base:
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or not json_string:
+            return []
+        return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of list_objs to a file"""
-
-        with open(f"{cls.__name__}.json", mode="a", encoding="utf-8") as fd:
-            if list_objs is None:
-                fd.write("[]")
-            else:
-                fd.write("[")
-                for index, obj in enumerate(list_objs):
-                    fd.write(cls.to_json_string(obj.to_dictionary()))
-                    if index < len(list_objs) - 1:
-                        fd.write(", ")
-                fd.write("]")
+        if list_objs is not None:
+            list_objs = [ob.to_dictionary() for ob in list_objs]
+        with open(f"{cls.__name__}.json", mode="w", encoding="utf-8") as fd:
+            fd.write(cls.to_json_string(list_objs))
