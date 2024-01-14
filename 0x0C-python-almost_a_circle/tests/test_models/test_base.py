d1c2dd337c5a8a3cae4d82d62e7382995d738521
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-14 15:25:38 +0100

    create the test case for checking if the class attr initialized to zero

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -15,7 +15,14 @@ class TestBase(unittest.TestCase):
         Base.__nb_objects = 0
 
     def test_if_base_class_has_private_attr(self):
+        """test if the base class has a private class attribute"""
         self.assertTrue(hasattr(Base, "_Base__nb_objects"))
+    
+    def test_if_private_attr_initialized_to_zero(self):
+        """tests if the nb_objects initialized to zero"""
+        self.assertEqual(Base.__nb_objects, 0)
+    
+    
 
 
 if __name__ == "__main__":
